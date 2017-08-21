1c9b7586cb595f53e9177c5d428874c09eac113a
天使也掉毛儿 <xiaodong137038@163.com>
2017-08-21 13:31:38 +0800

    update

---
+++ test/FengYun.NPoco.Data.Test/App_Start/NPocoDbConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FengYun.NPoco.Data;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(FengYun.NPoco.Data.Test.NPocoDbConfig), "Register")]
+namespace FengYun.NPoco.Data.Test
+{
+    /// <summary>
+    /// 初始化NPoco-ORM
+    /// </summary>
+    public class NPocoDbConfig
+    {
+        /// <summary>
+        /// 初始化NPoco-ORM数据库仓储
+        /// </summary>
+        public static void Register()
+        {
+            DefaultDbFactory.Setup();
+        }
+    }
+}
--- test/FengYun.NPoco.Data.Test/Properties/AssemblyInfo.cs
+++ test/FengYun.NPoco.Data.Test/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的一般信息由以下
+// 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("FengYun.NPoco.Data.Test")]
@@ -10,27 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FengYun.NPoco.Data.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("版权所有(C)  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-//将 ComVisible 设置为 false 将使此程序集中的类型
-//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
-//请将此类型的 ComVisible 特性设置为 true。
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的某个类型，
+// 请针对该类型将 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("2b527d93-a756-47f3-a4bf-b74dfef9d1ab")]
+[assembly: Guid("7c248456-7180-44dd-8a77-0b7e264c3c52")]
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
-//      生成号
+//      内部版本号
 //      修订号
 //
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
+// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用“*”:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
